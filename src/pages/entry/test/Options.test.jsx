ed5bfc05a9719a1761358c923d353026f3b0ecfc
HyungwonJin <hwjin92@gmail.com>
2023-03-08 15:08:54 +0900

    Code and test topping options images from server

--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-
 import Options from "../Options";
 
 test("displays image for each scoop option from server", async () => {
@@ -12,4 +11,18 @@ test("displays image for each scoop option from server", async () => {
   // confirm alt text of images
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("Displays image for each toppings option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const images = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  const imageTitles = images.map((img) => img.alt);
+  expect(imageTitles).toStrictEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });
